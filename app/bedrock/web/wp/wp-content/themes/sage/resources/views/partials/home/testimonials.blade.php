e106c6f4dd4cc1f405fcb6814a2a5992906687a5
Joseph Tyburski <66630934+jatyburski@users.noreply.github.com>
2020-12-28 20:02:21 -0500

    Started testimonials
    Created testimonial framework and restructured button styles

---
+++ app/bedrock/web/wp/wp-content/themes/sage/resources/views/partials/home/testimonials.blade.php
@@ -0,0 +1,28 @@
+{{-- Testimonials --}}
+
+<section class="row no-gutters team">
+	<div class="col-lg-8">
+		<h2 class="position-relative text-center accent team__title">
+			<span class="d-block font-weight-bold text-uppercase">{{ the_field('subtitle') }}</span>
+			{{ the_field('title') }}
+		</h2>
+
+		<div class="col-lg-6">
+			<img src="" alt="" />
+		</div>
+
+		<ul class="carousel-inner col-lg-6 m-0 p-0 position-relative">
+
+			@php $slides = get_field( 'testimonials' ) @endphp
+	
+			@foreach ( $slides as $index => $slide )
+				<li class="carousel-item{{ $index == 0 ? ' active' : '' }}">
+					<blockquote>
+						{{ $slide[ 'quote' ] }}
+					</blockquote>
+				</li>
+			@endforeach
+	
+		</ul>
+	</div>
+</section>
